mcastorina/poster

commit a7c0abcfef1608f84801a1ad6e329ce1564a6148

Add support for request variables
In order to support request variables, I also had to change the Run and
RunEnv functions to return a *http.Response which resulted in removing
the flags argument.

Also, an infinite loop formed where GenerateVariable called Run which
called GenerateVarible (and so on). This commit includes a temporary fix
via a duplicated internal run function that will not generate
RequestType variables. This will also fail a request containing a
variable that depends on a request that depends on another request
variable (i.e. run req1 ---> gen var1 ---> run req2 -X->  gen var2).

internal/models/models.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -12,13 +13,10 @@ import (
 	"strings"
 
 	"github.com/mcastorina/poster/internal/store"
+	"github.com/yalp/jsonpath"
 )
 
 const (
-	FlagPrintResponseCode = uint32(0x1 << 0)
-	FlagPrintHeaders      = uint32(0x1 << 1)
-	FlagPrintBody         = uint32(0x1 << 2)
-
 	ConstType   = "const"
 	RequestType = "request"
 	ScriptType  = "script"
@@ -30,8 +28,8 @@ type Resource interface {
 }
 
 type Runnable interface {
-	Run(flags uint32) error
-	RunEnv(env Environment, flags uint32) error
+	Run() (*http.Response, error)
+	RunEnv(env Environment) (*http.Response, error)
 }
 
 // Header
@@ -54,15 +52,22 @@ type Request struct {
 	Headers     []Header    `yaml:"headers"`
 }
 
-func (r *Request) Run(flags uint32) error {
-	return r.RunEnv(r.Environment, flags)
+// TODO: These private functions are for request variables
+//       to avoid infinite loops.
+//       Adding a lastGenerated attribute may solve this.
+func (r *Request) run() (*http.Response, error) {
+	return r.runEnv(r.Environment)
 }
-func (r *Request) RunEnv(e Environment, flags uint32) error {
+func (r *Request) runEnv(e Environment) (*http.Response, error) {
 	// Generate variables
 	for _, variable := range e.GetVariables() {
+		// Skip request variables to avoid infinite loop
+		if variable.Type == RequestType {
+			continue
+		}
 		if err := variable.GenerateValue(); err != nil {
 			// TODO: log error
-			return err
+			return nil, err
 		}
 	}
 
@@ -74,7 +79,7 @@ func (r *Request) RunEnv(e Environment, flags uint32) error {
 	req, err := http.NewRequest(method, url, strings.NewReader(body))
 	if err != nil {
 		// TODO: log error
-		return err
+		return nil, err
 	}
 	// Add headers
 	for _, header := range r.Headers {
@@ -87,27 +92,47 @@ func (r *Request) RunEnv(e Environment, flags uint32) error {
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		// TODO: log error
-		return err
+		return nil, err
+	}
+	return resp, nil
+}
+
+func (r *Request) Run() (*http.Response, error) {
+	return r.RunEnv(r.Environment)
+}
+func (r *Request) RunEnv(e Environment) (*http.Response, error) {
+	// Generate variables
+	for _, variable := range e.GetVariables() {
+		if err := variable.GenerateValue(); err != nil {
+			// TODO: log error
+			return nil, err
+		}
 	}
 
-	if flagIsSet(FlagPrintResponseCode, flags) {
-		fmt.Printf("%s %s\n", resp.Proto, resp.Status)
+	method := e.ReplaceVariables(r.Method)
+	url := e.ReplaceVariables(r.URL)
+	body := e.ReplaceVariables(r.Body)
+
+	// Create request
+	req, err := http.NewRequest(method, url, strings.NewReader(body))
+	if err != nil {
+		// TODO: log error
+		return nil, err
 	}
-	if flagIsSet(FlagPrintHeaders, flags) {
-		for header, values := range resp.Header {
-			fmt.Printf("%s: %s\n", header, strings.Join(values, ","))
-		}
-		fmt.Println()
+	// Add headers
+	for _, header := range r.Headers {
+		key := e.ReplaceVariables(header.Key)
+		value := e.ReplaceVariables(header.Value)
+		req.Header.Add(key, value)
 	}
-	if flagIsSet(FlagPrintBody, flags) {
-		body, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			return err
-		}
-		fmt.Printf("%s", body)
+
+	// Send request and get response
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		// TODO: log error
+		return nil, err
 	}
-	return nil
+	return resp, nil
 }
 func (r *Request) Save() error {
 	if err := r.Validate(); err != nil {
@@ -148,10 +173,6 @@ func (r *Request) Validate() error {
 	}
 	r.URL = urlObj.String()
 	return nil
-}
-
-func flagIsSet(flag, flags uint32) bool {
-	return flag&flags != 0
 }
 
 // Environment
@@ -219,11 +240,17 @@ func (e *Environment) ReplaceVariables(input string) string {
 
 // Variable
 type Variable struct {
-	Name        string      `yaml:"name"`
-	Value       string      `yaml:"value"`
-	Environment Environment `yaml:"environment"`
-	Type        string      `yaml:"type"`
-	Generator   string      `yaml:"generator"`
+	Name        string             `yaml:"name"`
+	Value       string             `yaml:"value"`
+	Environment Environment        `yaml:"environment"`
+	Type        string             `yaml:"type"`
+	Generator   *VariableGenerator `yaml:"generator,omitempty"`
+}
+
+type VariableGenerator struct {
+	RequestName string `yaml:"request-name,omitempty"`
+	RequestPath string `yaml:"request-path,omitempty"`
+	Script      string `yaml:"script,omitempty"`
 }
 
 func (v *Variable) Save() error {
@@ -253,7 +280,7 @@ func (v *Variable) GenerateValue() error {
 	case ConstType:
 		return nil
 	case ScriptType:
-		out, err := exec.Command("bash", "-c", v.Generator).Output()
+		out, err := exec.Command("bash", "-c", v.Generator.Script).Output()
 		if err != nil {
 			return err
 		}
@@ -266,7 +293,47 @@ func (v *Variable) GenerateValue() error {
 		}
 		return nil
 	case RequestType:
-		return fmt.Errorf("not implemented")
+		req, err := GetRequestByName(v.Generator.RequestName)
+		if err != nil {
+			return err
+		}
+		// TODO: add runenv option
+		resp, err := req.run()
+		if err != nil {
+			return err
+		}
+		// read body
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return err
+		}
+
+		// assign to variable
+		if v.Generator.RequestPath == "" {
+			v.Value = string(bytes.Trim(body, "\n"))
+		} else {
+			var jBody interface{}
+			if err := json.Unmarshal(body, &jBody); err != nil {
+				return nil
+			}
+			val, err := jsonpath.Read(jBody, v.Generator.RequestPath)
+			if err != nil {
+				return err
+			}
+			if value, ok := val.(string); !ok {
+				return fmt.Errorf("the JSONPath points to a non-string value: %+v\n", val)
+			} else {
+				v.Value = value
+			}
+		}
+		// TODO: This function should not have to write the result to store,
+		//       however, it is necessary at this point in order for the changes
+		//       to be used elsewhere
+		if err := v.ToStore().Update(); err != nil {
+			return err
+		}
+		return nil
 	}
 	return ErrorInvalidType
 }

go.mod
@@ -12,9 +12,11 @@ require (
 	github.com/mattn/go-sqlite3 v1.11.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v0.0.5
+	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.3.2
 	github.com/stretchr/testify v1.4.0
 	github.com/urfave/cli v1.22.1
+	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0
 	github.ibm.com/IAM/uum v0.0.0-20190927184355-9ee975de411d // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gopkg.in/yaml.v2 v2.2.2

go.sum
@@ -167,6 +167,8 @@ github.com/ulule/limiter v0.0.0-20180219101415-54a63f76da48/go.mod h1:VJx/ZNGmCl
 github.com/urfave/cli v1.22.1 h1:+mkCCcOFKPnCmVYVcURKps1Xe+3zP90gSYGNfRkjoIY=
 github.com/urfave/cli v1.22.1/go.mod h1:Gos4lmkARVdJ6EkW0WaNv/tZAAMe9V7XWyB60NtXRu0=
 github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77/go.mod h1:aYKd//L2LvnjZzWKhF00oedf4jCCReLcmhLdhm1A27Q=
+github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 h1:6fRhSjgLCkTD3JnJxvaJ4Sj+TYblw757bqYgZaOq5ZY=
+github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0/go.mod h1:/LWChgwKmvncFJFHJ7Gvn9wZArjbV5/FppcK2fKk/tI=
 github.com/yuin/gopher-lua v0.0.0-20190206043414-8bfc7677f583/go.mod h1:gqRgreBUhTSL0GeU64rtZ3Uq3wtjOa/TB2YfrtkCbVQ=
 github.ibm.com/IAM/PEP_go_lib v0.0.0-20181016193937-837b86b1dcb6 h1:1OQbTV8Dj3ObCKCkM+k8/oqSG/V2KD1aFT/SNEf6wUU=
 github.ibm.com/IAM/PEP_go_lib v0.0.0-20181016193937-837b86b1dcb6/go.mod h1:HVxM7wnEcK0N745HZ1FJ5TeiNS5zjQQY6eoSviEG2x0=

internal/cli/create.go
@@ -88,6 +88,25 @@ A variable resource contains the following attributes:
 	Run:  createScriptVariable,
 	Args: createScriptVariableArgs,
 }
+var createRequestVariableCmd = &cobra.Command{
+	Use:     "request-variable NAME REQUEST",
+	Aliases: []string{"request-var", "req-variable", "req-var", "rv"},
+	Short:   "Create a request variable resource",
+	Long: `Create request-variable will create and save a request variable resource.
+Variables in a request are denoted by prefixing the name with a colon
+(e.g. :variable-name).
+
+A variable resource contains the following attributes:
+
+    name                Name of the variable
+    value               Current value of the variable
+    type                Type of variable (const, request, script)
+    environment         Environment this variable belongs to
+    generator           How to generate the value
+`,
+	Run:  createRequestVariable,
+	Args: createRequestVariableArgs,
+}
 
 func init() {
 	rootCmd.AddCommand(createCmd)
@@ -95,6 +114,7 @@ func init() {
 	createCmd.AddCommand(createEnvironmentCmd)
 	createCmd.AddCommand(createConstVariableCmd)
 	createCmd.AddCommand(createScriptVariableCmd)
+	createCmd.AddCommand(createRequestVariableCmd)
 
 	// create flags
 	createCmd.PersistentFlags().BoolP("interactive", "i", false, "Interactively create the resource")
@@ -110,6 +130,10 @@ func init() {
 
 	// create script-variable flags
 	createScriptVariableCmd.Flags().StringP("environment", "e", "", "Environment to store variable in")
+
+	// create request-variable flags
+	createRequestVariableCmd.Flags().StringP("environment", "e", "", "Environment to store variable in")
+	createRequestVariableCmd.Flags().StringP("jsonpath", "j", "", "JSONPath to extract the value from the result body")
 }
 
 // run functions
@@ -185,11 +209,30 @@ func createScriptVariable(cmd *cobra.Command, args []string) {
 		Name:        args[0],
 		Type:        models.ScriptType,
 		Environment: models.Environment{Name: environment},
-		Generator:   args[1],
+		Generator: &models.VariableGenerator{
+			Script: args[1],
+		},
 	}
-	if err := variable.GenerateValue(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: could not generate value: %+v\n", err)
+	if err := variable.Save(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: could not save variable: %+v\n", err)
 		os.Exit(1)
+	}
+}
+func createRequestVariable(cmd *cobra.Command, args []string) {
+	if interactive, _ := cmd.Flags().GetBool("interactive"); interactive {
+		createRequestVariableI(cmd, args)
+		return
+	}
+	environment, _ := cmd.Flags().GetString("environment")
+	jPath, _ := cmd.Flags().GetString("jsonpath")
+	variable := &models.Variable{
+		Name:        args[0],
+		Type:        models.RequestType,
+		Environment: models.Environment{Name: environment},
+		Generator: &models.VariableGenerator{
+			RequestName: args[1],
+			RequestPath: jPath,
+		},
 	}
 	if err := variable.Save(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: could not save variable: %+v\n", err)
@@ -276,6 +319,19 @@ func createScriptVariableArgs(cmd *cobra.Command, args []string) error {
 	}
 	if len(args) != 2 {
 		return fmt.Errorf("expected args missing: NAME GENERATOR")
+	}
+	if !flagsAreSet(cmd, "environment") {
+		// TODO: make const
+		return fmt.Errorf("expected flag missing: --environment")
+	}
+	return nil
+}
+func createRequestVariableArgs(cmd *cobra.Command, args []string) error {
+	if interactive, _ := cmd.Flags().GetBool("interactive"); interactive {
+		return nil
+	}
+	if len(args) != 2 {
+		return fmt.Errorf("expected args missing: NAME REQUEST")
 	}
 	if !flagsAreSet(cmd, "environment") {
 		// TODO: make const
@@ -384,7 +440,36 @@ func createScriptVariableI(cmd *cobra.Command, args []string) {
 		Name:        "script variable template",
 		Environment: models.Environment{Name: "local"},
 		Type:        models.ScriptType,
-		Generator:   `date +'%D %T'`,
+		Generator: &models.VariableGenerator{
+			Script: `date +'%D %T'`,
+		},
+	}
+	var err error
+	data, _ := yaml.Marshal(template)
+	data, err = updateData(data)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: failed to create variable: %+v\n", err)
+		os.Exit(1)
+	}
+	if err := yaml.Unmarshal([]byte(data), &template); err != nil {
+		fmt.Fprintf(os.Stderr, "error: failed to create variable: %+v\n", err)
+		os.Exit(1)
+	}
+	if err := template.Save(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: failed to create variable: %+v\n", err)
+		os.Exit(1)
+	}
+}
+func createRequestVariableI(cmd *cobra.Command, args []string) {
+	// TODO: move this to models
+	template := models.Variable{
+		Name:        "request variable template",
+		Environment: models.Environment{Name: "local"},
+		Type:        models.RequestType,
+		Generator: &models.VariableGenerator{
+			RequestName: "request-name",
+			RequestPath: "$",
+		},
 	}
 	var err error
 	data, _ := yaml.Marshal(template)

internal/cli/run.go
@@ -2,7 +2,10 @@ package cli
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/mcastorina/poster/internal/models"
 	"github.com/spf13/cobra"
@@ -43,25 +46,54 @@ func run(cmd *cobra.Command, args []string) {
 		}
 	}
 
-	flags := models.FlagPrintResponseCode
-	if printHeaders, _ := cmd.Flags().GetBool("verbose"); printHeaders {
-		flags |= models.FlagPrintHeaders
-		flags |= models.FlagPrintBody
-	}
+	verboseFlag, _ := cmd.Flags().GetBool("verbose")
 
 	for _, arg := range args {
-		if resource, err := models.GetRunnableResourceByName(arg); err == nil {
-			if env.Name == "" {
-				// Use default environment
-				err = resource.Run(flags)
-			} else {
-				// Override environment
-				err = resource.RunEnv(env, flags)
-			}
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "error: %+v\n", err)
-				os.Exit(1)
-			}
+		resource, err := models.GetRunnableResourceByName(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "eror: could not run %s: %+v\n", arg, err)
+			os.Exit(1)
 		}
+
+		var resp *http.Response
+		if env.Name == "" {
+			// Use default environment
+			resp, err = resource.Run()
+		} else {
+			// Override environment
+			resp, err = resource.RunEnv(env)
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: could not run %s: %+v\n", arg, err)
+			os.Exit(1)
+		}
+
+		printResponse(resp, verboseFlag)
 	}
+}
+
+// helper functions
+func printResponse(resp *http.Response, verbose bool) error {
+	// print response code
+	fmt.Printf("%s %s\n", resp.Proto, resp.Status)
+
+	// print headers
+	if verbose {
+		for header, values := range resp.Header {
+			fmt.Printf("%s: %s\n", header, strings.Join(values, ","))
+		}
+		fmt.Println()
+	}
+
+	// print body
+	if verbose {
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s", body)
+	}
+
+	return nil
 }

internal/models/convert.go
@@ -1,7 +1,11 @@
 package models
 
-import "github.com/mcastorina/poster/internal/store"
-import "strings"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mcastorina/poster/internal/store"
+)
 
 func (r *Request) ToStore() *store.Request {
 	headerStrings := []string{}
@@ -46,20 +50,39 @@ func convertToEnvironment(s store.Environment) Environment {
 }
 
 func (v *Variable) ToStore() *store.Variable {
+	generator := ""
+	switch v.Type {
+	case ScriptType:
+		generator = v.Generator.Script
+	case RequestType:
+		generator = fmt.Sprintf("%s:%s", v.Generator.RequestName, v.Generator.RequestPath)
+	}
 	return &store.Variable{
 		Name:        v.Name,
 		Value:       v.Value,
 		Environment: v.Environment.Name,
 		Type:        v.Type,
-		Generator:   v.Generator,
+		Generator:   generator,
 	}
 }
 func convertToVariable(s store.Variable) Variable {
-	return Variable{
+	variable := Variable{
 		Name:        s.Name,
 		Value:       s.Value,
 		Environment: Environment{Name: s.Environment},
 		Type:        s.Type,
-		Generator:   s.Generator,
 	}
+	generator := &VariableGenerator{}
+	switch variable.Type {
+	case ScriptType:
+		generator.Script = s.Generator
+	case RequestType:
+		namePath := strings.SplitN(s.Generator, ":", 2)
+		generator.RequestName = namePath[0]
+		generator.RequestPath = namePath[1]
+	case ConstType:
+		generator = nil
+	}
+	variable.Generator = generator
+	return variable
 }

commit 5a6bc5a1e405fce785b674e26dad8fe5f7ed4572

Add Environment.GetVariablesInRequest
This function will scan all the replaceable parts of the Request and
return the variables that are used. This cleans up the infinite loop
introduced in a7c0abcfef1608f84801a1ad6e329ce1564a6148, but it still
does not prevent a dependency cycle from occurring.

internal/models/models.go
@@ -52,58 +52,13 @@ type Request struct {
 	Headers     []Header    `yaml:"headers"`
 }
 
-// TODO: These private functions are for request variables
-//       to avoid infinite loops.
-//       Adding a lastGenerated attribute may solve this.
-func (r *Request) run() (*http.Response, error) {
-	return r.runEnv(r.Environment)
-}
-func (r *Request) runEnv(e Environment) (*http.Response, error) {
-	// Generate variables
-	for _, variable := range e.GetVariables() {
-		// Skip request variables to avoid infinite loop
-		if variable.Type == RequestType {
-			continue
-		}
-		if err := variable.GenerateValue(); err != nil {
-			log.Errorf("%+v\n", err)
-			return nil, err
-		}
-		variable.Save()
-	}
-
-	method := e.ReplaceVariables(r.Method)
-	url := e.ReplaceVariables(r.URL)
-	body := e.ReplaceVariables(r.Body)
-
-	// Create request
-	req, err := http.NewRequest(method, url, strings.NewReader(body))
-	if err != nil {
-		log.Errorf("%+v\n", err)
-		return nil, err
-	}
-	// Add headers
-	for _, header := range r.Headers {
-		key := e.ReplaceVariables(header.Key)
-		value := e.ReplaceVariables(header.Value)
-		req.Header.Add(key, value)
-	}
-
-	// Send request and get response
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Errorf("%+v\n", err)
-		return nil, err
-	}
-	return resp, nil
-}
-
 func (r *Request) Run() (*http.Response, error) {
+	// TODO: Check for dependency cycles
 	return r.RunEnv(r.Environment)
 }
 func (r *Request) RunEnv(e Environment) (*http.Response, error) {
 	// Generate variables
-	for _, variable := range e.GetVariables() {
+	for _, variable := range e.GetVariablesInRequest(r) {
 		if err := variable.GenerateValue(); err != nil {
 			log.Errorf("%+v\n", err)
 			return nil, errorGenerateVariableFailed
@@ -239,6 +194,31 @@ func (e *Environment) ReplaceVariables(input string) string {
 
 	return output
 }
+func (e *Environment) GetVariablesInRequest(r *Request) []Variable {
+	// Map of valid variable names
+	validVariables := make(map[string]Variable)
+	for _, variable := range e.GetVariables() {
+		validVariables[variable.Name] = variable
+	}
+	// Build search string as a combination of all parts
+	// of the request that can be replaced
+	searchString := r.Method + "\n" + r.URL + "\n" + r.Body
+	for _, header := range r.Headers {
+		searchString = searchString + "\n" + header.Key + "\n" + header.Value
+	}
+
+	// Search for variables in the string and add to slice
+	// if it is a valid variable name
+	variables := []Variable{}
+	re := regexp.MustCompile(`:(.*)\b`)
+	for _, varGroup := range re.FindAllStringSubmatch(searchString, -1) {
+		varName := varGroup[1]
+		if variable, ok := validVariables[varName]; ok {
+			variables = append(variables, variable)
+		}
+	}
+	return variables
+}
 
 // Variable
 type Variable struct {
@@ -293,8 +273,7 @@ func (v *Variable) GenerateValue() error {
 		if err != nil {
 			return err
 		}
-		// TODO: add runenv option
-		resp, err := req.run()
+		resp, err := req.Run()
 		if err != nil {
 			return err
 		}
@@ -311,7 +290,7 @@ func (v *Variable) GenerateValue() error {
 		} else {
 			var jBody interface{}
 			if err := json.Unmarshal(body, &jBody); err != nil {
-				return nil
+				return err
 			}
 			val, err := jsonpath.Read(jBody, v.Generator.RequestPath)
 			if err != nil {